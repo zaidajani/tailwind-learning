9b42c69f018d6b666e67e84865b2b7c469a3a48f
zaidajani <zaidajani2007@gmail.com>
2023-12-18 10:45:55 +0530

    Communications

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ReactComponent as House } from "./img/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <nav className="relative container mx-auto p-6">
+          <div className="flex items-center justify-between">
+            <div className="pt-2">
+              <House />
+            </div>
+            <div className="hidden md:flex space-x-6">
+              <a href="#" className="hover:text-darkGrayishBlue">
+                Pricing
+              </a>
+              <a href="#" className="hover:text-darkGrayishBlue">
+                Product
+              </a>
+              <a href="#" className="hover:text-darkGrayishBlue">
+                About us
+              </a>
+              <a href="#" className="hover:text-darkGrayishBlue">
+                Careers
+              </a>
+              <a href="#" className="hover:text-darkGrayishBlue">
+                Community
+              </a>
+            </div>
+            <div className="p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight">
+              Get Started
+            </div>
+          </div>
+        </nav>
+      </div>
+    </>
   );
 }
 
